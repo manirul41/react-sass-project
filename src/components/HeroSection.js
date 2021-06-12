ef2f3c4e1d4617acb1758bff62a0cf8bfe2cfb01
Manirul Islam Tanim <manirul41@gmail.com>
2021-06-12 21:59:44 +0600

    test image loading issue.

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,7 +5,7 @@ function HeroSection() {
     return (
         <div
             className="hero-container"
-            style={{ background: `url("/images/img-home.jpg") center center/cover no-repeat` }}
+            style={{ background: `url("/images/img-home.jp2") center center/cover no-repeat` }}
         >
             {/* <video src="/videos/video-1.mp4" autoPlay loop muted /> */}
             <h1>GO BEYOND</h1>
